50ebe90b6ed5a38d8c47c91261991e8c0042d520
Cedric Lisima <7251423+opii972@users.noreply.github.com>
2021-07-05 13:52:20 +0200

    feat: update folder structure + add parcel + add gh-pages

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ let gameStateArray;
 const compareCardArray = [];
 
 const generateCardsArray = totalCells => {
-  const arr = Array(totalCells).fill(null).map((_, i) => Math.floor(i/2));
+  const arr = Array(totalCells).fill(null).map((_, i) => Math.floor(i / 2));
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
-}
+};
 
 const handleCardClick = event => {
   if (isCardAnimationRunning || compareCardArray.length > 1) return;
 
-  const index = +event.target.closest('.card').getAttribute("data-id");
+  const index = +event.target.closest('.card').getAttribute('data-id');
 
   if (compareCardArray.includes(index) || gameStateArray[index]) return;
 
@@ -40,85 +40,72 @@ const handleCardClick = event => {
       }, 1200);
     }
   }
-}
+};
 
 const checkGameState = () => {
   if (gameStateArray.every(state => state)) {
     setTimeout(() => alert('You win!'), 600);
   }
-}
+};
 
 const shakeCard = cardIndex => {
-  setTimeout(() => document.querySelector(`[data-id='${cardIndex}']`).classList.add("card--shaked"), 600);
-  setTimeout(() => document.querySelector(`[data-id='${cardIndex}']`).classList.remove("card--shaked"), 1200);
-}
+  setTimeout(() => document.querySelector(`[data-id='${cardIndex}']`).classList.add('card--shaked'), 600);
+  setTimeout(() => document.querySelector(`[data-id='${cardIndex}']`).classList.remove('card--shaked'), 1200);
+};
 
 const flipCard = cardIndex => {
   const cardElement = document.querySelector(`[data-id='${cardIndex}']`);
 
-  if (!cardElement.classList.contains("card--selected")) {
-    cardElement.classList.add("card--selected");
-    cardElement.querySelector(".card-back").style.backgroundImage = cardCharacterMapper(gameCardsArray[cardIndex]);
+  if (!cardElement.classList.contains('card--selected')) {
+    cardElement.classList.add('card--selected');
+    cardElement.querySelector('.card-back').classList.add(`card--${gameCardsArray[cardIndex]}`);
   } else {
-    cardElement.classList.remove("card--selected");
-    cardElement.querySelector(".card-back").removeAttribute("style")
+    cardElement.classList.remove('card--selected');
+    cardElement.querySelector('.card-back').classList.remove(`card--${gameCardsArray[cardIndex]}`);
   }
-}
-
-const cardCharacterMapper = cardIndex => {
-  switch(cardIndex) {
-    case 0: return "url('file:images/ryu.jpg')";
-    case 1: return "url('file:images/ken.jpg')";
-    case 2: return "url('file:images/chun-li.jpg')";
-    case 3: return "url('file:images/honda.jpg')";
-    case 4: return "url('file:images/balrog.jpg')";
-    case 5: return "url('file:images/sagat.jpg')";
-    case 6: return "url('file:images/vega.jpg')";
-    case 7: return "url('file:images/bison.jpg')";
-  }
-}
+};
 
 const drawBoardGame = totalCells => {
   let index = 0;
-  while(index < totalCells) {
-    const cardFrontElement = document.createElement("div");
-    cardFrontElement.classList.add("card-front");
 
-    const cardBackElement = document.createElement("div");
-    cardBackElement.classList.add("card-back");
-    cardBackElement.style.backgroundImage = cardCharacterMapper(gameCardsArray[index]);
+  while (index < totalCells) {
+    const cardFrontElement = document.createElement('div');
+    cardFrontElement.classList.add('card-front');
 
-    const cardInnerElement = document.createElement("div");
-    cardInnerElement.classList.add("card-inner");
+    const cardBackElement = document.createElement('div');
+    cardBackElement.classList.add('card-back');
+
+    const cardInnerElement = document.createElement('div');
+    cardInnerElement.classList.add('card-inner');
     cardInnerElement.appendChild(cardFrontElement);
     cardInnerElement.appendChild(cardBackElement);
 
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.classList.add("card--selected");
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
     cardElement.setAttribute('data-id', index);
     cardElement.addEventListener('click', handleCardClick);
     cardElement.appendChild(cardInnerElement);
 
-    document.querySelector("#container").appendChild(cardElement);
-
+    document.querySelector('.scene').appendChild(cardElement);
     index++;
   }
-}
+};
 
 const startGame = () => {
+  gameCardsArray.forEach((_, cardIndex) => flipCard(cardIndex));
   isCardAnimationRunning = true;
+
   setTimeout(() => {
-    gameCardsArray.forEach((_,cardIndex) => flipCard(cardIndex));
+    gameCardsArray.forEach((_, cardIndex) => flipCard(cardIndex));
     isCardAnimationRunning = false;
   }, 2000);
-}
+};
 
 const loadGame = numberOfPairCards => {
   gameCardsArray = generateCardsArray(numberOfPairCards * 2);
   gameStateArray = new Array(numberOfPairCards * 2).fill(false);
   drawBoardGame(numberOfPairCards * 2);
-}
+};
 
 loadGame(8);
 startGame();
